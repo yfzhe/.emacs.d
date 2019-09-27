1f8656207cb6ce4e0786536867ae43444a3b5f48
yfzhe <yfangzhe@gmail.com>
2019-09-27 14:56:45 +0800

    the second step on org

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,7 +1,12 @@
 ;;; init-org.el
 
+(defvar org-directory '("~/Documents/Docs/org/"))
 (defvar org-agenda-files '("~/Documents/Docs/org/todos/"))
 
 (global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c l") 'org-store-link)
+
+;; TODO: should read this article and follow it on day
+;; https://orgmode.org/worg/org-configs/org-customization-guide.html
 
 (provide 'init-org)
