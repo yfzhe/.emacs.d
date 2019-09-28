887fce69aba92b4277b8facbf22ef9d1c0fb5c81
yfzhe <yfangzhe@gmail.com>
2019-09-28 13:59:37 +0800

    continue on js and ts

--- lisp/init-frontend.el
+++ lisp/init-frontend.el
@@ -8,16 +8,26 @@
   (setq-default js2-mode-show-parse-errors nil)
   (setq-default js2-mode-show-strict-warnings nil))
 
-(use-package elm-mode
-  :mode "\\.elm\\'")
 
 (use-package typescript-mode
-  :mode ("\\.ts\\'" "\\.tsx\\'"))
+  :mode ("\\.ts\\'" "\\.tsx\\'")
+  :config
+  (setq-default typescript-indent-level 2))
 
 (use-package tide
   :after (typescript-mode company flycheck)
   :hook ((typescript-mode . tide-setup)
 	 (typescript-mode . tide-hl-identifier-mode)
-	 (before-save . tide-format-beforce-save)))
+	 (before-save . tide-format-beforce-save))
+  :config
+  (setq company-tooltip-align-annotations t)
+  (setq tide-format-options
+	(list :indentSize 2
+	      :tabSize 2
+	      :insertSpaceAfterFunctionKeywordForAnonymousFunctions t
+	      :placeOpenBraceOnNewLineForFunctions nil)))
+
+(use-package elm-mode
+  :mode "\\.elm\\'")
 
 (provide 'init-frontend)
