bfab3c550831db404b8ae67d713e1802f3969a25
yfzhe <yfangzhe@gmail.com>
2019-09-29 22:21:30 +0800

    use `use-package` in init-flycheck.el

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -1,20 +1,17 @@
 ;;; init-flycheck.el
 ;;; Config on flycheck
 
-(require 'flycheck)
+(use-package flycheck
+  :hook (after-init . global-flycheck-mode)
+  :config
+  (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
 
-(add-hook 'after-init-hook #'global-flycheck-mode)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
 
-(setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
-
-;; only check while saving and opening files
-;;(setq flycheck-check-syntax-automatically '(save mode-enabled))
-
-(setq flycheck-indication-mode 'left-fringe)
-(when (fboundp 'define-fringe-bitmap)
-  (define-fringe-bitmap 'flycheck-fringe-bitmap-double-arrow
-    ;; [16 48 112 240 112 48 16] ;; this is right fringe
-    [48 56 60 62 60 56 48] nil nil 'center))
+  (setq flycheck-indication-mode 'left-fringe)
+  (when (fboundp 'define-fringe-bitmap)
+    (define-fringe-bitmap 'flycheck-fringe-bitmap-double-arrow
+      ;; [16 48 112 240 112 48 16] ;; this is right fringe
+      [48 56 60 62 60 56 48] nil nil 'center)))
 
 (provide 'init-flycheck)
-
