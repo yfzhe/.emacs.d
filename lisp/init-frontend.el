952e1ae3fbea027f2fe19976b2d8b52324f897a6
yfzhe <yfangzhe@gmail.com>
2019-09-30 10:41:40 +0800

    finally support tsx

--- lisp/init-frontend.el
+++ lisp/init-frontend.el
@@ -8,24 +8,30 @@
   (setq-default js2-mode-show-parse-errors nil)
   (setq-default js2-mode-show-strict-warnings nil))
 
-
 (use-package typescript-mode
-  :mode ("\\.ts\\'" "\\.tsx\\'")
+  :mode "\\.ts\\'"
   :config
   (setq-default typescript-indent-level 2))
 
+(use-package rjsx-mode
+  :mode "\\.\\(jsx\\|tsx\\)\\'")
+
 (use-package tide
-  :after (typescript-mode company flycheck)
+  ;; :after (:all (:any typescript-mode rjsx-mode) (:all company flycheck))
   :hook ((typescript-mode . tide-setup)
-	 (typescript-mode . tide-hl-identifier-mode)
-	 (before-save . tide-format-beforce-save))
+	 (rjsx-mode . tide-setup))
   :config
+  (tide-hl-identifier-mode)
   (setq company-tooltip-align-annotations t)
   (setq tide-format-options
 	(list :indentSize 2
 	      :tabSize 2
 	      :insertSpaceAfterFunctionKeywordForAnonymousFunctions t
 	      :placeOpenBraceOnNewLineForFunctions nil)))
+
+;; add node_modules into PATH, necessary for using eslint, etc.
+(use-package add-node-modules-path
+  :hook (js-mode js2-mode typescript-mode rjsx-mode))
 
 (use-package elm-mode
   :mode "\\.elm\\'")
