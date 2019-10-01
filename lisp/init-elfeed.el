ffe68fbc134a75992a8f42b6944b342bea863d44
yfzhe <yfangzhe@gmail.com>
2019-10-02 06:57:37 +0800

    start using emacs as rss reader

--- lisp/init-elfeed.el
+++ lisp/init-elfeed.el
@@ -4,12 +4,12 @@
 (use-package elfeed
   :bind (("C-c f" . elfeed))
   :config
-  (setq elfeed-db-directory (expand-file-name ".elfeed" user-emacs-directory))
-  (setq elfeed-feeds
-        '("http://planet.emacsen.org/atom.xml"
-          "http://www.masteringemacs.org/feed/"
-          "https://oremacs.com/atom.xml"
-          "https://pinecast.com/feed/emacscast"
-          "https://www.reddit.com/r/emacs.rss")))
+  (setq elfeed-db-directory "~/.emacs.d/.elfeed")
+  (setq-default elfeed-search-filter "@1-week-ago +unread +must"))
+
+(use-package elfeed-org
+  :config
+  (setq rmh-elfeed-org-files '("~/.emacs.d/elfeed.org"))
+  (elfeed-org))
 
 (provide 'init-elfeed)
