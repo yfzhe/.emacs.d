e8ee3e1a6dc30862bb9f13f1e4582afb3b223ed3
yfzhe <yfangzhe@gmail.com>
2019-10-02 21:43:53 +0800

    adjust init-spell.el

--- lisp/init-spell.el
+++ lisp/init-spell.el
@@ -6,30 +6,22 @@
 ;; - https://joelkuiper.eu/spellcheck_emacs
 ;; - https://github.com/redguardtoo/emacs.d/blob/master/lisp/init-spelling.el
 
-(setq ispell-program-name "hunspell")
-(setq ispell-local-dictionary "en_US")
-(setq ispell-local-dictionary-alist
-      '(("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil ("-d" "en_US") nil utf-8)))
-(setq ispell-hunspell-dictionary-alist ispell-local-dictionary-alist)
+(use-package flyspell
+  :if (executable-find "hunspell")
+  :hook (((text-mode org-mode markdown-mode git-commit-setup) . flyspell-mode)
+         ((js-mode js2-mode rjxs-mode typescript-mode) . flyspell-prog-mode))
+  :config
+  (setq ispell-program-name "hunspell")
+  (setq ispell-local-dictionary "en_US")
+  (setq ispell-local-dictionary-alist
+        '(("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil ("-d" "en_US") nil utf-8)))
+  (setq ispell-hunspell-dictionary-alist ispell-local-dictionary-alist))
 
-;; add hooks (flyspell-mode and flyspell-prog-mode)
-(defvar text-modes-needing-flyspell
-  '(text-mode-hook
-    markdown-mode-hook
-    git-commit-setup-hook))
-
-(defvar prog-modes-needing-flyspell
-  '(js-mode js2-mode rjxs-mode typescript-mode
-    racket-mode))
-
-(dolist (hook text-modes-needing-flyspell)
-  (add-hook hook (lambda () (flyspell-mode 1))))
-(dolist (hook prog-modes-needing-flyspell)
-  (add-hook hook 'flyspell-prog-mode))
-
-;; use package "flyspell-correct" for better correcting experience
-(require 'flyspell-correct-ivy)
-(setq flyspell-correct-interface #'flyspell-correct-ivy)
-(define-key flyspell-mode-map (kbd "C-c c") #'flyspell-correct-wrapper)
+;; for better correcting experience
+(use-package flyspell-correct-ivy
+  :bind (;;:map flyspell-mode
+         ("C-c c" .  flyspell-correct-wrapper))
+  :init
+  (setq flyspell-correct-interface #'flyspell-correct-ivy))
 
 (provide 'init-spell)
