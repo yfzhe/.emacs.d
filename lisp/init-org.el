3538327d83b0dfd7524a180e9275a448504aa8dd
yfzhe <yfangzhe@gmail.com>
2019-10-10 18:01:25 +0800

    more org-mode config

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,15 +1,23 @@
 ;;; init-org.el
 
+;;; some great resources
+;;; - https://orgmode.org  (of course!)
+;;; - https://orgmode.org/worg/org-tutorials/index.html (a list of tutorials)
+;;; - https://orgmode.org/worg/org-configs/org-customization-guide.html
+
 (setq org-directory '("~/Documents/Docs/org/"))
 (setq org-agenda-files '("~/Documents/Docs/org/todos/"))
-
-(setq show-week-agenda-p t)
-(setq org-log-done 'time)
 
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c l") 'org-store-link)
 
-;; TODO: should read this article and follow it on day
-;; https://orgmode.org/worg/org-configs/org-customization-guide.html
+(setq show-week-agenda-p t)
+(setq org-todo-keywords
+      '((sequence "TODO" "DOING" "|" "DONE" "CANCEL")))
+(setq org-log-done 'time)
+;;(setq org-agenda-skip-scheduled-if-done t)
+;;(setq org-agenda-skip-deadline-if-done t)
+
+(setq org-return-follows-link t)
 
 (provide 'init-org)
