16e3cc7e90eb2124b62d95807c4e6f0ddfae729b
yfzhe <yfangzhe@gmail.com>
2019-10-10 10:01:46 +0800

    eldoc-box (still unused)

--- init.el
+++ init.el
@@ -72,6 +72,9 @@
 ;;; eldoc
 ;; (use-package eldoc-box
 ;;   :after eldoc
-;;   :hook ((eldoc-mode . eldoc-box-hover-mode)
-;;          (eldoc-box-hover-mode . eldoc-box-hover-at-point-mode)))
+;;   :hook (((typescript-mode rjsx-mode) . eldoc-box-hover-mode)
+;;          (eldoc-box-hover-mode . eldoc-box-hover-at-point-mode))
+;;   :config
+;;   (set-face-attribute 'eldoc-box-body nil :background "#343645")
+;;   (setq eldoc-box-max-pixel-width 500))
 
