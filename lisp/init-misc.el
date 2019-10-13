47a2ece53b9eb76a7c5afefaa83b4218b00b843b
yfzhe <yfangzhe@gmail.com>
2019-10-14 01:03:23 +0800

    new init-util.el

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -1,23 +1,10 @@
 ;;; init-util.el   -*- lexical-binding: t -*-
 ;;; Make life easier
 
-(require 'dash)
-
-(defvar shrink-bases
-  `(("~" . ,(expand-file-name "~"))
-    ("~/D" . "~/Documents")
-    ("*Dejavu-Code*:" . "~/D/Dejavu/Code/")
-    ("*Org*:" . "~/D/Docs/Org/"))
-  "a list of path shrink-base: (care for the order!)")
-
-(defun yfzhe:shrink-path (full-path)
-  (-reduce-from (lambda (path base)
-                  (replace-regexp-in-string
-                   (concat "^" (cdr base)) (car base) path))
-                full-path
-                shrink-bases))
-
 ;;; echo file name
+(eval-when-compile
+  (require 'init-util))
+
 (defun echo-file-name ()
   (interactive)
   (let ((msg
