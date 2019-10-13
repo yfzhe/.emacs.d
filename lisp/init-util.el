d505284ca8a0c3844ebc61271a2dce78a027b4e5
yfzhe <yfangzhe@gmail.com>
2019-10-13 13:22:15 +0800

    add `echo-file-name`

--- lisp/init-util.el
+++ lisp/init-util.el
@@ -1,14 +1,37 @@
 ;;; init-util.el
 ;;; Make life easier
 
-;; Quick Openning
+(require 'dash)
+
+(defvar shrink-bases
+  `(("~" . ,(expand-file-name "~"))))
+
+(defun yfzhe:shrink-path (full-path)
+  (-reduce-from (lambda (path base)
+                  (replace-regexp-in-string
+                   (concat "^" (cdr base)) (car base) full-path))
+                full-path
+                shrink-bases))
+
+;;; echo file name
+(defun echo-file-name ()
+  (interactive)
+  (let ((msg
+         (if (buffer-file-name)
+             (yfzhe:shrink-path (buffer-file-name))
+           (concat "Buffer " (buffer-name) " does not open a file!"))))
+    (message msg)))
+
+(global-set-key (kbd "C-c u f") 'echo-file-name)
+
+;; quick open ~/.emacs.d
 (defun open-emacs-d ()
   (interactive)
   (find-file "~/.emacs.d/"))
 
-;;; Need dictionary to save my poor english
+;; need dictionary to save my poor english
 (use-package youdao-dictionary
   :ensure t
-  :bind (("C-c y" . youdao-dictionary-search-at-point)))
+  :bind (("C-c u y" . youdao-dictionary-search-at-point)))
 
 (provide 'init-util)
