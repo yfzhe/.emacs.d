986c7f5262b52b391f0d9569e61c93aec984b444
yfzhe <yfangzhe@gmail.com>
2019-10-14 21:46:35 +0800

    use web-mode for jsx
    tsx-tide is not supported in rjsx-mode

--- lisp/init-frontend.el
+++ lisp/init-frontend.el
@@ -13,14 +13,23 @@
   :config
   (setq-default typescript-indent-level 2))
 
-(use-package rjsx-mode
-  :mode "\\.\\(js\\|jsx\\|tsx\\)\\'")
+;; it seems that tsx-tide is not supported in rjsx-mode,
+;; so we have to use web-mode for jsx (and tsx)
+(use-package web-mode
+  :mode "\\.\\(js\\|jsx\\|tsx\\)\\'"
+  :hook (web-mode . tide-setup)
+  :config
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
 
 (use-package tide
-  :after (:all (:any typescript-mode rjsx-mode) company flycheck)
-  :hook ((typescript-mode rjsx-mode) . tide-setup)
+  :after (:all (:any typescript-mode web-mode) company flycheck)
+  :hook ((typescript-mode web-mode) . tide-setup)
   :config
   (tide-hl-identifier-mode)
+  (flycheck-add-next-checker 'javascript-eslint 'jsx-tide 'append)
+  (flycheck-add-next-checker 'javascript-eslint 'tsx-tide 'append)
   (setq tide-format-options
         (list :indentSize 2
               :tabSize 2
@@ -29,12 +38,12 @@
 
 ;; add node_modules into PATH, necessary for using eslint, etc.
 (use-package add-node-modules-path
-  :hook (js-mode js2-mode typescript-mode rjsx-mode))
+  :hook (js-mode js2-mode typescript-mode web-mode))
 
 ;;; use `eldoc-box' for `tide' displaying signature
 (use-package eldoc-box
   :after eldoc
-  :hook (((typescript-mode rjsx-mode) . eldoc-box-hover-mode)
+  :hook (((typescript-mode web-mode) . eldoc-box-hover-mode)
          (eldoc-box-hover-mode . eldoc-box-hover-at-point-mode))
   :config
   (set-face-background 'eldoc-box-body "#343645")
