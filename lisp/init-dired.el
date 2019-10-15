99522c16555b8fe4ef8bf5fb29f1a08262f27deb
yfzhe <yfangzhe@gmail.com>
2019-10-15 18:28:24 +0800

    add dired-filter

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -33,6 +33,11 @@
   ;;   (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file))
   )
 
+(use-package dired-filter
+  ;; TODO: find out why `:bind` is failed here
+  :config
+  (define-key dired-mode-map (kbd "C-c f") 'dired-filter-mode))
+
 ;; colorize dired-mode
 (use-package diredfl
   :init (diredfl-global-mode 1))
