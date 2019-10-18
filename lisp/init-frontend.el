5411b9de83d78a9229052feafb4c5dd5ee02e23c
yfzhe <yfangzhe@gmail.com>
2019-10-18 14:58:36 +0800

    web-mode is horrible

--- lisp/init-frontend.el
+++ lisp/init-frontend.el
@@ -13,44 +13,50 @@
   :config
   (setq-default typescript-indent-level 2))
 
+;; use rjsx-mode for jsx
+;; (indentation in web-mode is too weird)
 (use-package rjsx-mode
-  :mode "\\.\\(js\\|jsx\\)\\'"
+  :mode "\\.\\(js\\|jsx\\|tsx\\)\\'"
   :hook (rjsx-mode . tide-setup))
 
-;; it seems that tsx-tide is not supported in rjsx-mode,
-;; so we have to use web-mode for tsx
-(use-package web-mode
-  :mode "\\.tsx\\'"
-  :hook (web-mode . tide-setup)
-  :config
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-css-indent-offset 2)
-  (setq web-mode-code-indent-offset 2))
-
 (use-package tide
-  :after (:all (:any typescript-mode rjsx-mode web-mode) company flycheck)
-  :hook ((typescript-mode rjsx-mode web-mode) . tide-setup)
+  :after (:all (:any typescript-mode rjsx-mode) company flycheck)
+  :hook ((typescript-mode rjsx-mode) . tide-setup)
   :bind (:map tide-mode-map
          ("C-c C-d" . tide-documentation-at-point))
   :config
   (tide-hl-identifier-mode)
-  (flycheck-add-next-checker 'javascript-eslint 'jsx-tide 'append)
-  (flycheck-add-next-checker 'javascript-eslint 'tsx-tide 'append)
-  (flycheck-add-next-checker 'typescript-tide 'javascript-eslint 'append)
+  ;; ts format settings
   (setq tide-format-options
         (list :indentSize 2
               :tabSize 2
               :insertSpaceAfterFunctionKeywordForAnonymousFunctions t
-              :placeOpenBraceOnNewLineForFunctions nil)))
+              :placeOpenBraceOnNewLineForFunctions nil))
+
+  ;; use both typescript typechecker and eslint as flycheck-checker
+  (flycheck-add-next-checker 'typescript-tide 'javascript-eslint 'append)
+  (flycheck-add-next-checker 'javascript-eslint 'jsx-tide 'append)
+  ;; (flycheck-add-next-checker 'javascript-eslint 'tsx-tide 'append)
+
+  ;; the flycheck-er `tsx-tide` does not support `rjsx-mode',
+  ;; so we have to make our one.
+  (flycheck-define-generic-checker 'tsx-tide-for-rjsx-mode
+    "A JSX syntax checker using tsserver, for `rjsx-mode'"
+    :start #'tide-flycheck-start
+    :verify #'tide-flycheck-verify
+    :modes '(rjsx-mode)
+    :predicate #'tide-flycheck-predicate)
+  (add-to-list 'flycheck-checkers 'tsx-tide-for-rjsx-mode t)
+  (flycheck-add-next-checker 'javascript-eslint 'tsx-tide-for-rjsx-mode 'append))
 
 ;; add node_modules into PATH, necessary for using eslint, etc.
 (use-package add-node-modules-path
-  :hook (js-mode js2-mode typescript-mode web-mode))
+  :hook (js-mode js2-mode typescript-mode rjsx-mode))
 
 ;;; use `eldoc-box' for `tide' displaying signature
 (use-package eldoc-box
   :after eldoc
-  :hook (((typescript-mode web-mode) . eldoc-box-hover-mode)
+  :hook (((typescript-mode rjsx-mode) . eldoc-box-hover-mode)
          (eldoc-box-hover-mode . eldoc-box-hover-at-point-mode))
   :config
   (set-face-background 'eldoc-box-body "#343645")
