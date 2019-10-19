0e304b8d3ae3e938a5bfab8a48b1cbf27b976d23
yfzhe <yfangzhe@gmail.com>
2019-10-20 01:31:43 +0800

    add a key-binding for useful `racket-check-syntax-mode`
    missing the arrows in DrRacket :(

--- lisp/init-racket.el
+++ lisp/init-racket.el
@@ -5,13 +5,15 @@
   :mode "\\.rkt\\'"
   :hook ((racket-mode . rainbow-delimiters-mode))
   :bind (:map racket-mode-map
+         ("C-c C-s" . racket-check-syntax-mode)
          (")" . racket-insert-closing)
          ("]" . racket-insert-closing)
          ("}" . racket-insert-closing)
          ("C-M-\\" . racket-unicode-input-method-enable)))
 
 (use-package scribble-mode
-  :mode "\\.scrbl\\'")
+  :mode "\\.scrbl\\'"
+  :hook (scribble-mode . racket-mode))
 
 (use-package pollen-mode
   :mode "\\.\\(p\\|pp\\|pm\\|pmd\\|poly\\|ptree\\)\\'"
