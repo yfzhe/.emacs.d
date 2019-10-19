b4a79c5b3158c5f83402732a7aa78c55768d087b
yfzhe <yfangzhe@gmail.com>
2019-10-20 04:24:18 +0800

    display emojis (on macos)
    use font "Segoe UI Emoji" on windows, but I have not tested it.

--- lisp/init-font.el
+++ lisp/init-font.el
@@ -11,13 +11,16 @@
 
 (set-frame-font (concat font-family " " (number-to-string font-size)))
 
+;; display emojis
+(when (eq system-type 'darwin)
+  (set-fontset-font t 'unicode "Apple Color Emoji" nil 'prepend))
+
+;; chinese font
 (dolist (charset '(kana han cjk-misc bopomofo))
   (set-fontset-font (frame-parameter nil 'font)
                     charset
                     (font-spec :family "Source Han Sans SC")))
 
 (use-cjk-char-width-table 'zh_CN)
-
-;; (set-fontset-font t 'unicode "Apple Color Emoji" nil 'prepend)
 
 (provide 'init-font)
