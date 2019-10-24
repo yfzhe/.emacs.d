ff03f9ba54d8f9bc014671d0aa0911c6c61cb2ff
yfzhe <yfangzhe@gmail.com>
2019-10-24 13:53:39 +0800

    add `j-mode`

--- init.el
+++ init.el
@@ -72,6 +72,17 @@
 (use-package rust-mode
   :mode "\\.rs\\'")
 
+(use-package j-mode
+  :bind (:map j-mode-map
+         ("C-c C-z" . j-console))
+  :config
+  (setq j-console-cmd "jconsole")
+  ;; FIXME: these faces need better customization
+  (setq j-verb-face font-lock-function-name-face)
+  (setq j-adverb-face font-lock-builtin-face)
+  (setq j-conjunction-face font-lock-keyword-face)
+  (setq j-other-face font-lock-constant-face))
+
 (use-package markdown-mode
   :commands (markdown-mode gfm-mode)
   :mode (("README\\.md\\'" . gfm-mode)
