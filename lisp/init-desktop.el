3cc14ad63c10eeaed4884d080018fd8770968991
yfzhe <yfangzhe@gmail.com>
2019-10-31 19:49:32 +0800

    add `eyebrowse`, and update config on desktop.el

--- lisp/init-desktop.el
+++ lisp/init-desktop.el
@@ -6,20 +6,18 @@
 ;;; - https://www.emacswiki.org/emacs/desktop
 
 (use-package desktop
-  :hook (kill-emacs . session-save)
+  :hook (after-init . desktop-save-mode)
   :config
-  (defun session-save ()
-    "A customized version of `desktop-save'"
-    (interactive)
-    (if (file-exists-p (expand-file-name desktop-base-file-name desktop-dirname))
-        (when (yes-or-no-p "Exists a previous saved desktop session, overwrite it?")
-          (desktop-save desktop-dirname))
-      (desktop-save desktop-dirname)))
-
   (setq desktop-restore-frames nil) ; disable restoring frames when initing
   (let ((dir (expand-file-name "desktop" user-emacs-directory)))
     (setq desktop-dirname dir)
     (setq desktop-path (list dir)))
   (setq desktop-buffers-not-to-save "\\(magit\\|tide\\)"))
+
+;;; use `eyebrowse' to control windows
+(use-package eyebrowse
+  :hook (after-init . eyebrowse-mode)
+  :init
+  (setq eyebrowse-keymap-prefix (kbd "C-c w")))
 
 (provide 'init-desktop)
