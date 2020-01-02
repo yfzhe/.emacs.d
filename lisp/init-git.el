22ee644f8fef01167d2a10e5db58a3ee2dbf6f6d
yfzhe <yfangzhe@gmail.com>
2020-01-02 19:03:28 +0800

    update: doc about magit-todos

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -5,9 +5,6 @@
          ("C-x M-g" . magit-dispatch)
          ("C-c M-g" . magit-file-popup)))
 
-;; `magit-todos' would open git-grep html manual on Windows, which is annoying.
-;; It is a known bug, see https://github.com/alphapapa/magit-todos/issues/43
-;; Before the issue get fixed, shoule we not use this package on Windows?
 (use-package magit-todos
   :hook (emacs-startup . magit-todos-mode))
 
