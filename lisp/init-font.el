239661f2d82b5d504cd36c04a7384d2ddd4efa64
yfzhe <yfangzhe@gmail.com>
2020-02-02 15:23:46 +0800

    update module init-font with the new `defvar/os` macro

--- lisp/init-font.el
+++ lisp/init-font.el
@@ -1,33 +1,37 @@
 ;;; init-font.el  -*- lexical-binding: t -*-
 ;;; Font
 
+(require 'init-util)
+
+(defvar/os font-size
+  :windows 11
+  :macos 15)
+
 ;; other great monospace fonts:
 ;; SF Mono, IBM Plex Mono, Input Mono
 ;; Iosevka, Operator Mono, Computer Modern Textwriter
 (defvar font-family "SF Mono")
 
-(defvar font-size
-  (pcase system-type
-    ('windows-nt 11)
-    ('darwin 15)))
+(defvar/os font-sans-family
+  :windows "思源黑体" ; what's the hell!
+  :default "Source Han Sans SC")
 
-(defvar font-cjk-family
-  (pcase system-type
-    ('windows-nt "思源黑体") ;; what's the hell!
-    ('darwin "Source Han Sans SC")))
+(defvar font-cjk-family font-sans-family)
+
+(defvar/os font-emoji-family
+  :macos "Apple Color Emoji"
+  :default nil)
 
 (let ((font-string (concat font-family " " (number-to-string font-size))))
   (set-face-attribute 'default nil :font font-string :weight 'semi-light))
 
 ;; display emojis
-(when (eq system-type 'darwin)
-  (set-fontset-font t 'unicode "Apple Color Emoji" nil 'prepend))
+(when font-emoji-family
+  (set-fontset-font t 'unicode font-emoji-family nil 'prepend))
 
 ;; chinese font
 (dolist (charset '(kana han cjk-misc bopomofo))
-  (set-fontset-font (frame-parameter nil 'font)
-                    charset
-                    (font-spec :family font-cjk-family)))
+  (set-fontset-font t charset font-cjk-family))
 
 (use-cjk-char-width-table 'zh_CN)
 
