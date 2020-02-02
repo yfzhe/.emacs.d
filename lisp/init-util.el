f311a51ac8cbdca24b2638b9be0c65f65863f2dc
yfzhe <yfangzhe@gmail.com>
2020-02-02 14:56:52 +0800

    update: add `value/os` macro in init-util

--- lisp/init-util.el
+++ lisp/init-util.el
@@ -3,6 +3,28 @@
 
 (require 'dash)
 
+;;; ----------------------------------------
+;;; abstract the `(pcase system-type ...)' pattern
+(defmacro value/os (&rest args)
+  "Value depended on `system-type',
+each clause using a keyword, `:windows', `:macos', or `:linux',
+and an optional `:default' clause."
+  (let ((windows (plist-member args :windows))
+        (macos (plist-member args :macos))
+        (linux (plist-member args :linux))
+        (default (plist-member args :default)))
+    `(pcase system-type
+       ,@(and windows `(('windows-nt ,(cadr windows))))
+       ,@(and macos `(('darwin ,(cadr macos))))
+       ,@(and linux `(('gnu/linux ,(cadr linux))))
+       ,@(and default `((t ,(cadr default)))))))
+
+(defmacro defvar/os (id &rest rhs)
+  "Define variable based on `system-type', see `value/os'."
+  `(defvar ,id (value/os ,@rhs)))
+
+;;; ----------------------------------------
+;;; shrink-path
 (defvar shrink-bases
   `(("~" . ,(expand-file-name "~"))
     ("~/D" . "~/Documents")
