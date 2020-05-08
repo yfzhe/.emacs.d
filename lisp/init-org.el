70da2a68a4e1b97300e82bd8839903976ff7d408
yfzhe <yfangzhe@gmail.com>
2020-05-08 19:28:16 +0800

    update: org config

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -5,10 +5,14 @@
 ;;; - https://orgmode.org/worg/org-tutorials/index.html (a list of tutorials)
 ;;; - https://orgmode.org/worg/org-configs/org-customization-guide.html
 
-(defvar my-org-agenda-files
-  (pcase system-type
-    ('windows-nt "C:/Dropbox/todos/")
-    ('darwin "~/Documents/Docs/org/todos/")))
+(require 'init-util)
+
+(defvar/os my-org-directory
+  :macos "~/Documents/Docs/org/")
+
+(defvar/os my-org-agenda-files
+  :windows "C:/Dropbox/todos/"
+  :macos "~/Documents/Docs/org/todos/")
 
 (use-package org
   :ensure nil
@@ -17,13 +21,14 @@
   :hook (; use auto-fill-mode when edit org files
          (org-mode . auto-fill-mode))
   :config
-  (setq org-directory '("~/Documents/Docs/org/"))
+  (setq org-directory (list my-org-directory))
   (setq org-agenda-files (list my-org-agenda-files))
 
-  (setq show-week-agenda-p t)
   (setq org-todo-keywords
         '((sequence "TODO" "DOING" "|" "DONE" "CANCEL")))
   (setq org-log-done 'time)
+
+  (setq show-week-agenda-p t)
   ;;(setq org-agenda-skip-scheduled-if-done t)
   ;;(setq org-agenda-skip-deadline-if-done t)
 
