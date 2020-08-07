11b67ea668ef5eba1609f72bb60c52d988d43c8e
yfzhe <yfangzhe@gmail.com>
2020-08-07 11:54:16 +0800

    init-font: use defconst

--- lisp/init-font.el
+++ lisp/init-font.el
@@ -3,25 +3,25 @@
 
 (require 'init-util)
 
-(defvar/os font-size
+(defconst/os font-size
   :windows 11
   :macos 15)
 
 ;; other great monospace fonts:
 ;; SF Mono, IBM Plex Mono, Input Mono
 ;; Iosevka, Operator Mono, Computer Modern Textwriter
-(defvar font-family "SF Mono")
+(defconst font-family "SF Mono")
 
-(defvar/os font-sans-family
+(defconst/os font-sans-family
   :windows "Arial"
   :macos "Helvetica Neue")
 ;; NOTE: macOS's default sans font is "PingFang SC", actually
 
-(defvar/os font-cjk-family
+(defconst/os font-cjk-family
   :windows "思源黑体" ; what's the hell!
   :default "Source Han Sans SC")
 
-(defvar/os font-emoji-family
+(defconst/os font-emoji-family
   :windows "Segoe UI Emoji"
   :macos "Apple Color Emoji"
   :default nil)
