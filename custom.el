66cfc2e82d6359f548b8c9257830fa83bf76b548
yfzhe <yfangzhe@gmail.com>
2020-08-11 18:22:10 +0800

    misc: custom.el update to emacs 27.1

--- custom.el
+++ custom.el
@@ -12,39 +12,35 @@
  '(beacon-color "#ed0547ad8099")
  '(company-quickhelp-color-background "#4F4F4F")
  '(company-quickhelp-color-foreground "#DCDCCC")
- '(compilation-message-face (quote default))
- '(custom-enabled-themes (quote (doom-dracula)))
+ '(compilation-message-face 'default)
+ '(custom-enabled-themes '(doom-dracula))
  '(custom-safe-themes
-   (quote
-    ("6a0d7f41968908e25b2f56fa7b4d188e3fc9a158c39ef680b349dccffc42d1c8" "912cac216b96560654f4f15a3a4d8ba47d9c604cbc3b04801e465fb67a0234f0" "53993d7dc1db7619da530eb121aaae11c57eaf2a2d6476df4652e6f0bd1df740" "1526aeed166165811eefd9a6f9176061ec3d121ba39500af2048073bea80911e" default)))
- '(evil-emacs-state-cursor (quote ("#E57373" hbar)))
- '(evil-insert-state-cursor (quote ("#E57373" bar)))
- '(evil-normal-state-cursor (quote ("#FFEE58" box)))
- '(evil-visual-state-cursor (quote ("#C5E1A5" box)))
+   '("57bd93e7dc5fbb5d8d27697185b753f8563fe0db5db245592bab55a8680fdd8c" "6a0d7f41968908e25b2f56fa7b4d188e3fc9a158c39ef680b349dccffc42d1c8" "912cac216b96560654f4f15a3a4d8ba47d9c604cbc3b04801e465fb67a0234f0" "53993d7dc1db7619da530eb121aaae11c57eaf2a2d6476df4652e6f0bd1df740" "1526aeed166165811eefd9a6f9176061ec3d121ba39500af2048073bea80911e" default))
+ '(evil-emacs-state-cursor '("#E57373" hbar))
+ '(evil-insert-state-cursor '("#E57373" bar))
+ '(evil-normal-state-cursor '("#FFEE58" box))
+ '(evil-visual-state-cursor '("#C5E1A5" box))
  '(fci-rule-character-color "#d9d9d9")
  '(fci-rule-color "#62686E")
- '(highlight-changes-colors (quote ("#FD5FF0" "#AE81FF")))
+ '(highlight-changes-colors '("#FD5FF0" "#AE81FF"))
  '(highlight-indent-guides-auto-enabled nil)
  '(highlight-symbol-colors
-   (quote
-    ("#FFEE58" "#C5E1A5" "#80DEEA" "#64B5F6" "#E1BEE7" "#FFCC80")))
+   '("#FFEE58" "#C5E1A5" "#80DEEA" "#64B5F6" "#E1BEE7" "#FFCC80"))
  '(highlight-symbol-foreground-color "#E0E0E0")
- '(highlight-tail-colors (quote (("#ed0547ad8099" . 0) ("#424242" . 100))))
- '(hl-paren-background-colors (quote ("#2492db" "#95a5a6" nil)))
- '(hl-paren-colors (quote ("#ecf0f1" "#ecf0f1" "#c0392b")))
+ '(highlight-tail-colors '(("#ed0547ad8099" . 0) ("#424242" . 100)))
+ '(hl-paren-background-colors '("#2492db" "#95a5a6" nil))
+ '(hl-paren-colors '("#ecf0f1" "#ecf0f1" "#c0392b"))
  '(hl-sexp-background-color "#1c1f26")
  '(jdee-db-active-breakpoint-face-colors (cons "#1c1f24" "#51afef"))
  '(jdee-db-requested-breakpoint-face-colors (cons "#1c1f24" "#7bc275"))
  '(jdee-db-spec-breakpoint-face-colors (cons "#1c1f24" "#484854"))
  '(magit-diff-use-overlays nil)
  '(nrepl-message-colors
-   (quote
-    ("#183691" "#888a88" "#539100" "#888a88" "#0086b3" "#183691" "#539100" "#888a88")))
+   '("#183691" "#888a88" "#539100" "#888a88" "#0086b3" "#183691" "#539100" "#888a88"))
  '(objed-cursor-color "#ff665c")
  '(package-selected-packages
-   (quote
-    (json-mode lsp-python-ms all-the-icons-ibuffer all-the-icons-ivy-rich lorem-ipsum countdown youdao-dictionary flyspell-correct-ivy elfeed-org elfeed deft dante haskell-mode j-mode cargo flycheck-rust rust-mode yarn-mode prettier-js add-node-modules-path tide web-mode rjsx-mode typescript-mode js2-mode counsel-css pollen-mode scribble-mode racket-mode company-lsp lsp-ui lsp-mode company-restclient restclient yaml-mode idris-mode elm-mode eldoc-box treemacs-projectile treemacs-magit ox-gfm ox-pandoc eshell-prompt-extras esh-autosuggest dired-git-info dired-subtree diredfl dired-filter yasnippet-snippets ivy-yasnippet yasnippet ibuffer-projectile projectile flycheck gitignore-mode gitconfig-mode gitattributes-mode git-link git-messenger git-timemachine diff-hl magit-todos magit company dashboard eyebrowse ivy-rich ivy-hydra counsel ivy goto-line-preview hl-todo dr-racket-like-unicode undo-tree rainbow-delimiters paredit smartparens editorconfig kaolin-themes doom-themes doom-modeline paradox link-hint which-key exec-path-from-shell use-package)))
- '(pdf-view-midnight-colors (quote ("#888a88" . "#edf5dc")))
+   '(js-mode json-mode lsp-python-ms all-the-icons-ibuffer all-the-icons-ivy-rich lorem-ipsum countdown youdao-dictionary flyspell-correct-ivy elfeed-org elfeed deft dante haskell-mode j-mode cargo flycheck-rust rust-mode yarn-mode prettier-js add-node-modules-path tide web-mode rjsx-mode typescript-mode js2-mode counsel-css pollen-mode scribble-mode racket-mode company-lsp lsp-ui lsp-mode company-restclient restclient yaml-mode idris-mode elm-mode eldoc-box treemacs-projectile treemacs-magit ox-gfm ox-pandoc eshell-prompt-extras esh-autosuggest dired-git-info dired-subtree diredfl dired-filter yasnippet-snippets ivy-yasnippet yasnippet ibuffer-projectile projectile flycheck gitignore-mode gitconfig-mode gitattributes-mode git-link git-messenger git-timemachine diff-hl magit-todos magit company dashboard eyebrowse ivy-rich ivy-hydra counsel ivy goto-line-preview hl-todo dr-racket-like-unicode undo-tree rainbow-delimiters paredit smartparens editorconfig kaolin-themes doom-themes doom-modeline paradox link-hint which-key exec-path-from-shell use-package))
+ '(pdf-view-midnight-colors '("#888a88" . "#edf5dc"))
  '(pos-tip-background-color "#3a933a933a93")
  '(pos-tip-foreground-color "#9E9E9E")
  '(red "#ffffff")
@@ -76,8 +72,7 @@
     (cons 360 "#62686E")))
  '(vc-annotate-very-old-color nil)
  '(weechat-color-list
-   (quote
-    (unspecified "#272822" "#3C3D37" "#F70057" "#F92672" "#86C30D" "#A6E22E" "#BEB244" "#E6DB74" "#40CAE4" "#66D9EF" "#FB35EA" "#FD5FF0" "#74DBCD" "#A1EFE4" "#F8F8F2" "#F8F8F0"))))
+   '(unspecified "#272822" "#3C3D37" "#F70057" "#F92672" "#86C30D" "#A6E22E" "#BEB244" "#E6DB74" "#40CAE4" "#66D9EF" "#FB35EA" "#FD5FF0" "#74DBCD" "#A1EFE4" "#F8F8F2" "#F8F8F0")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
