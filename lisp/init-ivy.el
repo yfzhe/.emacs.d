fd3d8ec5230e267a1b72d8e3195608a0739bd3ed
yfzhe <yfangzhe@gmail.com>
2020-08-20 14:12:41 +0800

    ivy: fuzzy matching

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -4,9 +4,18 @@
 (use-package ivy
   :hook (after-init . ivy-mode)
   :init
-  ;; (setq ivy-use-virtual-buffers t)
   (setq ivy-count-format "(%d/%d) ")
-  (setq ivy-height 8))
+  (setq ivy-height 8)
+
+  (setq ivy-use-virtual-buffers t)
+
+  ;; enable fuzzy matching
+  ;; https://oremacs.com/2016/01/06/ivy-flx/
+  ;; with `ivy-hydra', use "C-o M" to toggle
+  (setq ivy-re-builders-alist
+        '((swiper-isearch . ivy--regex-plus)
+          (swiper . ivy--regex-plus)
+          (t . ivy--regex-fuzzy))))
 
 (use-package counsel
   :hook (after-init . counsel-mode)
@@ -23,6 +32,9 @@
          :map isearch-mode-map
          ("M-s" . swiper-isearch-toggle)))
 
+;; fuzzy matching
+(use-package flx)
+
 (use-package ivy-hydra)
 
 ;;; better interface for ivy
@@ -32,10 +44,5 @@
 ;;; prettier ivy-rich
 (use-package all-the-icons-ivy-rich
   :init (all-the-icons-ivy-rich-mode 1))
-
-;;; another way to find files
-;; (use-package ivy-explorer
-;;   :after ivy
-;;   :config (ivy-explorer-mode 1))
 
 (provide 'init-ivy)
