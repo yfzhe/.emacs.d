772ec8acd43f3165674be7d59aedcb16b7372de9
yfzhe <yfangzhe@gmail.com>
2021-03-05 14:30:03 +0800

    lsp: remove headerline breadcrumb

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -9,6 +9,7 @@
   (setq lsp-prefer-flymake nil)
   (setq lsp-enable-indentation t)
   (setq lsp-enable-snippet t)
+  (setq lsp-headerline-breadcrumb-enable nil)
 
   (setq lsp-keymap-prefix "C-c l"))
 
@@ -17,7 +18,7 @@
   :init
   (setq lsp-ui-doc-use-webkit nil)
   (setq lsp-ui-doc-delay 0.5)
-  (setq lsp-ui-doc-max-width 50)
+  (setq lsp-ui-doc-max-width 56)
   ;; move signature info from eldoc to lsp-ui-doc
   (setq lsp-ui-doc-include-signature t)
   (setq lsp-eldoc-enable-hover nil)
