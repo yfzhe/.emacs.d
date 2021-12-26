150a3c28566d046f654f30a98f875a024a8949d8
yfzhe <yfangzhe@gmail.com>
2021-12-27 00:17:46 +0800

    org: some code update

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,6 +1,8 @@
 ;;; init-org.el
+;;; The most powerful GTD software, and
+;;; one of the most powerful document tool and system.
 
-;;; some great resources
+;;; Some great resources:
 ;;; - https://orgmode.org  (of course!)
 ;;; - https://orgmode.org/worg/org-tutorials/index.html (a list of tutorials)
 ;;; - https://orgmode.org/worg/org-configs/org-customization-guide.html
@@ -12,7 +14,7 @@
   :windows "C:/Dropbox"
   :macos "~/Documents/Docs/org")
 
-(defun my-org-file (path)
+(defun my-org-file-path (path)
   "build file-path based on `my-org-directory'"
   (expand-file-name path my-org-directory))
 
@@ -24,10 +26,11 @@
   :config
   ;; pathes
   (setq org-directory my-org-directory)
-  (setq org-agenda-files (list (my-org-file "todos")))
-  (setq org-default-notes-file (my-org-file "./todos/inbox.org"))
-  (let ((archive-file (my-org-file "./archive/todos.org")))
-    (setq org-archive-location (concat archive-file "::")))
+  (setq org-agenda-files (list (my-org-file-path "todos")))
+  (setq org-default-notes-file (my-org-file-path "./todos/inbox.org"))
+  (setq org-archive-location
+        (let ((archive-file (my-org-file-path "./archive/todos.org")))
+          (concat archive-file "::")))
 
   ;; basic settings
   (setq org-todo-keywords
