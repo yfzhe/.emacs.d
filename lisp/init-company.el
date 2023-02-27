289b5b08198c569d4ba996587b2991d7e148c992
yfzhe <yfangzhe@gmail.com>
2023-02-27 17:23:10 +0800

    company: disable `company-dabbrev-downcase`

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -1,16 +1,15 @@
 ;;; init-company.el
 ;;; Completaion and company-mode
 
-;; no, please keep the origin case
-;; (setq completion-ignore-case t)
-
 (use-package company
   :diminish company-mode
   :hook (after-init . global-company-mode)
   :config
   (setq company-minimum-prefix-length 2)
   (setq company-idle-delay 0.1)
-  (setq company-tooltip-align-annotations t))
+  (setq company-tooltip-align-annotations t)
+
+  (setq company-dabbrev-downcase nil))
 
 ;; (use-package company-box
 ;;   :hook (company-mode . company-box-mode))
