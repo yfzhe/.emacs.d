3018071340d8fef4b8cc10d77c6d8306a47801be
yfzhe <yfangzhe@gmail.com>
2023-03-03 17:24:16 +0800

    lsp: improve lsp-mode configuration

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -1,28 +1,30 @@
 ;;; init-lsp.el
 
-(use-package lsp-mode
-  :bind (:map lsp-mode-map
-         ("C-c C-d" . lsp-describe-thing-at-point))
-  :hook ((lsp-mode . lsp-enable-which-key-integration))
-  :init
-  (setq lsp-auto-guess-root t)
-  (setq lsp-prefer-flymake nil)
-  (setq lsp-enable-indentation t)
-  (setq lsp-enable-snippet t)
-  (setq lsp-headerline-breadcrumb-enable nil)
+(setq read-process-output-max (* 1 1024 1024))
 
-  (setq lsp-keymap-prefix "C-c l"))
+(use-package lsp-mode
+  :hook ((lsp-mode . lsp-enable-which-key-integration))
+  :bind (:map lsp-mode-map
+              ("C-c C-d" . lsp-describe-thing-at-point)
+              ([remap xref-find-definitions] . lsp-find-definition)
+              ([remap xref-find-references] . lsp-find-references))
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+
+  ;; (setq lsp-auto-guess-root t)
+  (setq lsp-enable-indentation t)
+  (setq lsp-enable-folding nil)
+  (setq lsp-enable-text-document-color nil)
+  (setq lsp-headerline-breadcrumb-enable nil))
 
 (use-package lsp-ui
   :commands lsp-ui-mode
   :init
-  (setq lsp-ui-doc-use-webkit nil)
   (setq lsp-ui-doc-delay 0.5)
   (setq lsp-ui-doc-max-width 56)
-  ;; move signature info from eldoc to lsp-ui-doc
   (setq lsp-ui-doc-include-signature t)
-  (setq lsp-eldoc-enable-hover nil)
 
+  (setq lsp-ui-sideline-show-hover nil)
   (setq lsp-ui-sideline-show-code-actions nil)
   (setq lsp-ui-sideline-show-diagnostics t))
 
