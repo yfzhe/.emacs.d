c5bdab2ac7f9c9e17743b09642e54aed0c81d149
yfzhe <yfangzhe@gmail.com>
2023-03-05 19:13:56 +0800

    package: remove `esh-autosuggest`

--- lisp/init-shell.el
+++ lisp/init-shell.el
@@ -6,18 +6,6 @@
   :init (setq vterm-always-compile-module t))
 
 ;;; eshell
-(use-package esh-autosuggest
-  :defines (ivy-display-functions-alist)
-  :hook ((eshell-mode . esh-autosuggest-mode)
-         (eshell-mode . setup-eshell-ivy-completion))
-  :preface
-  (defun setup-eshell-ivy-completion ()
-    (setq-local ivy-display-functions-alist
-                (remq (assoc 'ivy-completion-in-region ivy-display-functions-alist)
-                      ivy-display-functions-alist)))
-  :bind (:map eshell-mode-map
-              ([remap eshell-pcomplete] . completion-at-point)))
-
 (use-package eshell-prompt-extras
   :after esh-opt
   :defines (eshell-highlight-prompt eshell-prompt-function)
