fe6699b44d54e1970ffad4a832bf938f95b7c1cb
yfzhe <yfangzhe@gmail.com>
2023-03-06 16:08:55 +0800

    lsp: improve lsp-mode configuration, continued

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -5,24 +5,29 @@
 (use-package lsp-mode
   :hook ((lsp-mode . lsp-enable-which-key-integration))
   :bind (:map lsp-mode-map
-              ("C-c C-d" . lsp-describe-thing-at-point)
-              ([remap xref-find-definitions] . lsp-find-definition)
-              ([remap xref-find-references] . lsp-find-references))
+         ("C-c C-d" . lsp-describe-thing-at-point)
+         ([remap xref-find-definitions] . lsp-find-definition)
+         ([remap xref-find-references] . lsp-find-references))
   :init
   (setq lsp-keymap-prefix "C-c l")
 
-  ;; (setq lsp-auto-guess-root t)
   (setq lsp-enable-indentation t)
   (setq lsp-enable-folding nil)
   (setq lsp-enable-text-document-color nil)
-  (setq lsp-headerline-breadcrumb-enable nil))
+  (setq lsp-headerline-breadcrumb-enable nil)
+
+  (setq lsp-eldoc-enable-hover nil))
 
 (use-package lsp-ui
   :commands lsp-ui-mode
   :init
+  (setq lsp-ui-doc-enable t)
   (setq lsp-ui-doc-delay 0.5)
-  (setq lsp-ui-doc-max-width 56)
+  (setq lsp-ui-doc-max-width 72)
+  (setq lsp-ui-doc-max-height 20)
   (setq lsp-ui-doc-include-signature t)
+  (setq lsp-ui-doc-show-with-cursor t)
+  (setq lsp-ui-doc-show-with-mouse t)
 
   (setq lsp-ui-sideline-show-hover nil)
   (setq lsp-ui-sideline-show-code-actions nil)
