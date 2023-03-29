a4c6c5911b11a045ebb4b30d38fe9ad0067c8a22
yfzhe <yfangzhe@gmail.com>
2023-03-29 15:21:14 +0800

    company: no `company-idle-delay`

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -2,11 +2,10 @@
 ;;; completaion
 
 (use-package company
-  :diminish company-mode
   :hook (after-init . global-company-mode)
   :config
   (setq company-minimum-prefix-length 2)
-  (setq company-idle-delay 0.1)
+  (setq company-idle-delay 0)
   (setq company-tooltip-align-annotations t)
 
   (setq company-dabbrev-downcase nil))
