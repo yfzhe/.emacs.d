f01478a2f3e1618766209ba5510ebddb19166d84
yfzhe <yfangzhe@gmail.com>
2023-04-03 14:40:59 +0800

    consult: use `consult-buffer` and sister commands

--- lisp/init-vertico.el
+++ lisp/init-vertico.el
@@ -19,11 +19,20 @@
                                     orderless-prefixes)))
 
 (use-package consult
-  :bind (([remap recentf-open-files] . consult-recent-file)
+  :bind (([remap switch-to-buffer] . consult-buffer)
+         ([remap switch-to-buffer-other-window] . consult-buffer-other-window)
+         ([remap switch-to-buffer-other-frame] . consult-buffer-other-frame)
+         ([remap project-switch-to-buffer] . consult-project-buffer)
+         ([remap bookmark-jump] . consult-bookmark)
+         ([remap recentf-open-files] . consult-recent-file)
          ([remap goto-line] . consult-goto-line))
   :init
   (setq xref-show-definitions-function #'consult-xref)
-  (setq xref-show-xrefs-function #'consult-xref))
+  (setq xref-show-xrefs-function #'consult-xref)
+  :config
+  (consult-customize
+   consult-buffer consult-buffer-other-window consult-buffer-other-frame
+   consult-project-buffer :preview-key nil))
 
 (use-package embark
   :bind (("C-." . embark-act)
