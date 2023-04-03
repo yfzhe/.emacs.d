7057c43521bcf691454e3c3147d0a57df0858b72
yfzhe <yfangzhe@gmail.com>
2023-04-03 21:11:13 +0800

    note: remove `deft` package

--- lisp/init-note.el
+++ lisp/init-note.el
@@ -25,20 +25,4 @@
   :bind (("C-c d f" . consult-notes)
          ("C-c d s" . consult-notes-search-in-all-notes)))
 
-(use-package deft
-  :config
-  (setq deft-extensions '("txt" "md" "org"))
-  (setq deft-default-extension "org")
-  (setq deft-directory my-note-directory)
-  (setq deft-recursive t)
-
-  (setq deft-use-filename-as-title nil)
-  (setq deft-use-filter-string-for-filename t)
-  (setq deft-file-naming-rules
-        '(;(noslash . "-") ;; don't replace slash, so we can make notes in sub-dir easier
-          (nospace . "-")
-          (case-fn . downcase)))
-
-  (setq deft-auto-save-interval 30))
-
 (provide 'init-note)
