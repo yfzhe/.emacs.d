c9338e16d0b8390bce0fe0fe2f71f704f137c4c3
yfzhe <yfangzhe@gmail.com>
2023-08-07 16:17:19 +0800

    program: start to use tree-sitter

--- lisp/init-prog.el
+++ lisp/init-prog.el
@@ -1,6 +1,30 @@
 ;;; init-prog.el
 ;;; Packages about programming, and packages about languages
 
+(use-package treesit
+  :ensure nil
+  :config
+  (setq treesit-language-source-alist
+        '((c "https://github.com/tree-sitter/tree-sitter-c")
+          (cpp "https://github.com/tree-sitter/tree-sitter-cpp")
+          (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+          (html "https://github.com/tree-sitter/tree-sitter-html")
+          (css "https://github.com/tree-sitter/tree-sitter-css")
+          (js . ("https://github.com/tree-sitter/tree-sitter-javascript"
+                 "master" "src"))
+          (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript"
+                         "master" "typescript/src"))
+          (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript"
+                  "master" "tsx/src"))
+          (common-lisp "https://github.com/theHamsta/tree-sitter-commonlisp")
+          (csharp "https://github.com/tree-sitter/tree-sitter-c-sharp")
+          (rust "https://github.com/tree-sitter/tree-sitter-rust")
+          (make "https://github.com/alemuller/tree-sitter-make")
+          (cmake "https://github.com/uyha/tree-sitter-cmake")
+          (json "https://github.com/tree-sitter/tree-sitter-json")
+          (toml "https://github.com/tree-sitter/tree-sitter-toml")
+          (yaml "https://github.com/ikatyang/tree-sitter-yaml"))))
+
 ;;; languages
 (use-package elm-mode
   :hook ((elm-mode . lsp-deferred))
