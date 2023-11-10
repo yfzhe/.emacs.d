4bc302d18d3cd0d96956019f688ae2d857181978
yfzhe <yfangzhe@gmail.com>
2023-11-10 17:28:29 +0800

    racket: use `racket-hash-lang-mode`

--- lisp/init-racket.el
+++ lisp/init-racket.el
@@ -2,25 +2,20 @@
 ;;; Racket and its languages
 
 (use-package racket-mode
-  :mode "\\.rkt\\'"
-  :hook ((racket-mode . racket-xp-mode)
+  :mode (("\\.rkt\\'" . racket-mode)
+         ("\\.rhm\\'" . racket-hash-lang-mode)
+         ("\\.scrbl\\'" . racket-hash-lang-mode))
+  :hook (((racket-mode racket-hash-lang-mode) . racket-xp-mode)
          (racket-mode . paredit-mode)
-         ((racket-mode racket-repl-mode) . rainbow-delimiters-mode))
+         (racket-mode . rainbow-delimiters-mode))
   :bind (:map racket-mode-map
          (")" . racket-insert-closing)
          ("]" . racket-insert-closing)
          ("}" . racket-insert-closing)
-         ("C-M-\\" . racket-unicode-input-method-enable)
-         :map paredit-mode-map
-         ("M-[" . paredit-wrap-square)
-         ("M-{" . paredit-wrap-curly))
+         ("C-M-\\" . racket-unicode-input-method-enable))
   :config
   ;; don't show the pos-tip, either the pseudo tip
   (setq racket-show-functions '(racket-show-echo-area)))
-
-(use-package scribble-mode
-  :mode "\\.scrbl\\'"
-  :hook ((scribble-mode . rainbow-delimiters-mode)))
 
 (use-package pollen-mode
   :mode "\\.\\(p\\|pp\\|pm\\|pmd\\|poly\\|ptree\\)\\'"
